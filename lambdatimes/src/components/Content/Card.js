81f41466bda0b0d76f43ab3bb04e631c477590b9
Firealem Erko <firealemg@yahoo.com>
2019-05-03 13:08:28 -0400

    added styled components to Card

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+
+`;
+const HeadLinediv = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+const Authordiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+
+  span {
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+`;
+const ImgageContainer = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+
+  img {
+    width: 40px;
+  }
+`;
+
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <CardContainer>
+       <HeadLinediv>{props.headline}</HeadLinediv>
+       <Authordiv>
+        <ImgageContainer>
           <img src={props.img} />
-        </div>
+        </ImgageContainer>
         <span>By {props.author}</span>
-      </div>
-    </div>
+      </Authordiv>
+      </CardContainer>
   );
 };
 
